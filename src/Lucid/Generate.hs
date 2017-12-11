2130000b532f8abe84f43280616364cd47797c97
Oleg Tsybulskyi <alogic0@gmail.com>
2017-12-11 15:00:00 +0200

    escape \r

--- src/Lucid/Generate.hs
+++ src/Lucid/Generate.hs
@@ -130,6 +130,7 @@ fromHtml opts t
     escape '"'  = "\\\""
     escape '\n' = "\\n"
     escape '\t' = "\\t"
+    escape '\r' = "\\r"
     escape '\\' = "\\\\"
     escape x    = [x]
 -- fromHtml _ (Comment comment) = map ("-- " ++) $ lines comment
